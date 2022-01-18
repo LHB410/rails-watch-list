c451f70761bc165c748edcf84d9bb22f23c25a0f
Laura Brooks <laura.brooks0@outlook.com>
2022-01-18 20:47:45 +0900

    delete links working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'lists#index'
+  resources :lists do
+    resources :bookmarks, only: [ :new, :create, :destroy ]
+  end
+
+
+
+
+  # get 'lists/index', to: 'lists#index'
+  # get 'lists/show', to: 'lists#show'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists/create', to: 'lists#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "json"
+require "rest-client"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,30 +8,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# json = RestClient.get "http://www.omdbapi.com/?apikey=[97689314]&" https://omdbapi.com/?t=star+wars&apikey=97689314
-# poster_json = RestClient.get "http://img.omdbapi.com/?apikey=[97689314]&"
+# json = RestClient.get "http://www.omdbapi.com/?apikey=[97689314]&" "https://omdbapi.com/?t=#{Faker::Movie.title}&apikey=97689314"
 # movie_info = JSON.parse(json)
-# poster_info = JSON.parse(poster_json)
-
-# pages.each do |page|
-#   json = RestClient.get ""
-#   item_info = JSON.parse(json)["results"]
-#     item_info.each do |item|
-#       MagicItem.create(
-#       name: item["name"],
-#       description: item["desc"],
-#       item_type: item["type"],
-#       rarity: item["rarity"],
-#       attunement: item["requires_attunement"]
-#     )
-#     end
-  # end
-  puts "Cleaning database..."
+puts "Cleaning database..."
+Bookmark.destroy_all
 Movie.destroy_all
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
+json = RestClient.get "http://tmdb.lewagon.com/movie/top_rated"
+movie_info = JSON.parse(json)["results"]
+  movie_info.each do |info|
+    Movie.create(
+    title: info["title"],
+    overview: info["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{info["poster_path"]}",
+    rating:  info["vote_average"]
+  )
+  end
 puts "Finished!"
